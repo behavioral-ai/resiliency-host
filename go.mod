89dc262d647c3bee6344a213fe5c8d5c86610c7d
advanced-go <mark.wilson.go@outlook.com>
2025-06-15 07:07:16 -0500

    Refactoring

--- go.mod
+++ go.mod
@@ -5,14 +5,14 @@ go 1.23.0
 //toolchain go1.23.7
 
 require (
-	github.com/behavioral-ai/collective v0.0.0-20250418132141-97418856c102
-	github.com/behavioral-ai/resiliency v0.0.0-20250418141007-feb6e480cb9c
+	github.com/behavioral-ai/collective v0.0.0-20250614222355-9da1545ab888
+	github.com/behavioral-ai/resiliency v0.0.0-20250614224749-e1652b75c99e
 )
 
 require (
-	github.com/behavioral-ai/core v0.0.0-20250418123820-bd6e8e92d701 // indirect
+	github.com/behavioral-ai/core v0.0.0-20250614220351-99a9d26282a5 // indirect
 	github.com/behavioral-ai/intermediary v0.0.0-20250418134848-083cc251bc9a // indirect
-	github.com/behavioral-ai/traffic v0.0.0-20250418134734-fb30499c124d // indirect
+	github.com/behavioral-ai/traffic v0.0.0-20250614224521-e4b9733a9197 // indirect
 	github.com/dchest/siphash v1.2.3 // indirect
 	github.com/dustinxie/lockfree v0.0.0-20210712051436-ed0ed42fd0d6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/behavioral-ai/collective/eventing"
+	"github.com/behavioral-ai/core/messaging"
 	"github.com/behavioral-ai/resiliency/endpoint"
 	"github.com/behavioral-ai/resiliency/operations"
 	"log"
@@ -15,17 +16,18 @@ import (
 )
 
 const (
-	portKey                 = "PORT"
-	addr                    = "0.0.0.0:8081"
-	writeTimeout            = time.Second * 300
-	readTimeout             = time.Second * 15
-	idleTimeout             = time.Second * 60
-	healthLivelinessPattern = "/health/liveness"
-	healthReadinessPattern  = "/health/readiness"
+	portKey      = "PORT"
+	addr         = "0.0.0.0:8081"
+	writeTimeout = time.Second * 300
+	readTimeout  = time.Second * 15
+	idleTimeout  = time.Second * 60
+	//healthLivelinessPattern = "/health/liveness"
+	//healthReadinessPattern  = "/health/readiness"
 
-	agentsConfigPath  = "file://[cwd]/resource/agents-config.txt"
-	originConfigPath  = "file://[cwd]/resource/origin-config.txt"
-	loggingConfigPath = "file://[cwd]/resource/logging-operators.json"
+	operatorsFileName = "logging-operators.json"
+	appFileName       = "app-config.json"
+	originFileName = "origin-config.json"
+	configSubDir = "/resource"
 )
 
 func main() {
@@ -82,26 +84,63 @@ func displayRuntime(port string) {
 	//fmt.Printf("env     : %v\n", core.EnvStr())
 }
 
+func readFile(fileName string) ([]byte, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	return os.ReadFile(dir + configSubDir + "/" + fileName)
+}
+
+var (
+	m2 = map[string]string{
+		messaging.RegionKey: "us-west1",
+		messaging.ZoneKey:    "oregon",
+		messaging.SubZoneKey: "portland",
+		messaging.InstanceIdKey: "123456789",
+	}
+)
+
 func startup(r *http.ServeMux, cmdLine []string) (http.Handler, bool) {
-	// Initialize eventing notifier and activity
-	operations.ConfigureEventing(eventing.OutputNotify, eventing.OutputActivity)
-	err := operations.ConfigureLogging(loggingConfigPath, originConfigPath)
+	// configure the Origin
+	err := operations.ConfigureOrigin(m2, func() ([]byte, error) {
+		return readFile(originFileName)
+	})
 	if err != nil {
-		fmt.Printf("%v\n", err)
+		fmt.Printf("error on Origin configuration: %v\n",err)
+		return r, false
 	}
-	err = operations.ConfigureAgents(agentsConfigPath, "")
+
+	// configure logging operators
+	err = operations.ConfigureLogging(func() ([]byte, error) {
+		return readFile(operatorsFileName)
+	})
 	if err != nil {
-		fmt.Printf("%v\n", err)
+		fmt.Printf("error on logging operators configuration: %v\n",err)
+		return r, false
 	}
+
+	// configure the application
+	appCfg,err1 := operations.ReadAppConfig(map[string]string,func() ([]byte, error) {
+		return readFile(appFileName)
+	})
+	if err1 != nil {
+		fmt.Printf("error on application configuration: %v\n",err1)
+		return r, false
+	}
+
+	// configure the application networks
+	errs := operations.ConfigureNetworks(appCfg,readFile)
+	if len(errs) > 0 {
+		fmt.Printf("error on application networks configuration: %v\n",errs)
+		return r, false
+	}
+
+	// initialize Http handlers
 	r.Handle("/favicon.ico", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
-
-	// Initialize health handlers
-	r.Handle(healthLivelinessPattern, endpoint.Health) //http.HandlerFunc(healthLivelinessHandler))
-	r.Handle(healthReadinessPattern, endpoint.Health)  //http.HandlerFunc(healthReadinessHandler))
-
-	// Operations and default http handler
-	r.Handle(endpoint.OperationsPattern, endpoint.Operations) //http.HandlerFunc(endpoint.Operations.Exchange))
-	r.Handle(endpoint.RootPattern, endpoint.Root)             //http.HandlerFunc(endpoint.Root.Exchange))
+	for _,v := range operations.Endpoint {
+		r.Handle(v.Pattern(),v)
+	}
 	return r, true
 }
 
@@ -124,5 +163,12 @@ func writeHealthResponse(w http.ResponseWriter, status *messaging.Status) {
 	}
 }
 
+// Initialize health handlers
+	//r.Handle(healthLivelinessPattern, endpoint.Health) //http.HandlerFunc(healthLivelinessHandler))
+	//r.Handle(healthReadinessPattern, endpoint.Health)  //http.HandlerFunc(healthReadinessHandler))
+
+	// Operations and default http handler
+	//r.Handle(endpoint.OperationsPattern, endpoint.Operations) //http.HandlerFunc(endpoint.Operations.Exchange))
+	//r.Handle(endpoint.RootPattern, endpoint.Root)             //http.HandlerFunc(endpoint.Root.Exchange))
 
 */
